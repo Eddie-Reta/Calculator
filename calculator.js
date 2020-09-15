ed0f18f4a86bfb78025deb2d2f4ef37cded60b53
eduardo reta rj <https://github.com/Eddie-Reta>
2020-09-15 19:45:03 -0400

    Structured folders.

---
+++ calculator.js
@@ -0,0 +1,15 @@
+//** Server Side Calculator */
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("<h1>Hello World.</h1>")
+});
+
+app.listen(3000, function(){
+    
+    console.log("Server is running on PORT 3000.");
+
+})
