009b880f74c431a5875e83d29dbde5161e0cd31b
eduardo reta rj <https://github.com/Eddie-Reta>
2020-09-15 21:26:24 -0400

    Post method added to pass in data to server.

--- calculator.js
+++ calculator.js
@@ -1,11 +1,48 @@
 //** Server Side Calculator */
 
 const express = require("express");
+//body-parser will let access the data that was posted in my site.
+const bodyParser = require("body-parser");
 
 const app = express();
 
+//You can use it with express by using use. 
+//allows us to post nested objects
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
-    res.send("<h1>Hello World.</h1>")
+
+    //__dirname will find file anywhere you have it.
+    res.sendFile(__dirname + "/client/index.html")
+});
+
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/client/bmiCalculator.html")
+});
+
+app.post("/", function(req, res) {
+
+        //with body parser you can get acces to req.body
+
+        //Number will turn a string into a number.
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    res.send("The result is " + result)
+});
+
+app.post("/bmicalculator", function(req, res) {
+    
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    
+    var bmi = weight / (height * height)
+
+
+    res.send("Your BMI is " + bmi);
+
 });
 
 app.listen(3000, function(){
@@ -13,3 +50,7 @@ app.listen(3000, function(){
     console.log("Server is running on PORT 3000.");
 
 })
+// function bmiCalculator(weight,height) {
+//     var bmi = weight / (height * height);
+//     return bmi;
+//     }
